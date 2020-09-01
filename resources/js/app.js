e93872949c458a6580f8dde4bc0c85b5bc427d53
Oliver <draper87@gmail.com>
2020-09-01 16:51:28 +0200

    quarto commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,27 @@ require('./bootstrap');
 
 var $ = require('jquery');
 
+const Handlebars = require("handlebars");
+
 $(document).ready(function() {
-  alert('Ciaociao');
+  $.ajax({
+    url: 'http://127.0.0.1:8000/api/students',
+    method: 'GET',
+    success: function(dataResponse) {
+      var studenti = dataResponse.students;
+
+      var source = document.getElementById("student-template").innerHTML;
+      var template = Handlebars.compile(source);
+
+
+      for (var i = 0; i < studenti.length; i++) {
+        var studente = studenti[i];
+        var html = template(studente);
+        $('.studente').append(html);
+      }
+    },
+    error: function error() {
+      alert('error');
+    }
+  });
 });
--- resources/views/students-handlebars.blade.php
+++ resources/views/students-handlebars.blade.php
@@ -1,15 +1,13 @@
 <h2>Lista Studenti</h2>
 
-{{-- <div class="studente">
+<div class="studente"></div>
 
-</div>
-
-<script id="entry-template" type="text/x-handlebars-template">
+<script id="student-template" type="text/x-handlebars-template">
   <ul>
       <li>Nome @{{ nome }}</li>
       <li>Cognome @{{ cognome }}</li>
       <li>Voto @{{ voto }}</li>
   </ul>
-</script> --}}
+</script>
 
-<script src="{{ assett('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
